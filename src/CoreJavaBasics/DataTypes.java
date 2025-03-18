9ce85b0a38a33db0fa1226eb8faa006347330d7b
Maddineni <maddinenisasidhar@gmail.com>
2025-03-18 07:04:35 +0530

    DataTypes

---
+++ src/CoreJavaBasics/DataTypes.java
@@ -0,0 +1,27 @@
+package CoreJavaBasics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int a=10;
+        short s=1;
+        long l= 80L;
+        char c='A';
+        byte b=1;
+        boolean bl=false;
+        float f= 30.0F;
+        double d=56.20D;
+
+        System.out.println("Value of int is"+a);
+        System.out.println("Value of short is"+s);
+        System.out.println("Value of long is"+l);
+        System.out.println("Value of char is"+c);
+        System.out.println("Value of byte is"+b);
+        System.out.println("Value of boolean is"+bl);
+        System.out.println("Value of float is"+f);
+        System.out.println("Value of double is"+d);
+
+
+
+    }
+}
